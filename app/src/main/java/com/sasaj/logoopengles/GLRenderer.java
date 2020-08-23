c1122275648b864d433139b47260d4e6898e6920
Sasa Jugurdzija <sasa.jugurdzija@bitpanda.com>
2020-08-23 10:06:27 +0200

    Add GLView.java and GLRenderer.java

---
+++ app/src/main/java/com/sasaj/logoopengles/GLRenderer.java
@@ -0,0 +1,24 @@
+package com.sasaj.logoopengles;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
+}
